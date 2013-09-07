59632ed72541bb5397658fc02ed390f6fd539cbd
Ashley Sarver <asarver1@gmail.com>
2013-09-07 02:17:29 -0400

    added a how many players screen as well as a choose character screen.

--- src/com/gradugation/MainActivity.java
+++ src/com/gradugation/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gradugation;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void newGameButtonClick(View view) {
+    	Intent intent = new Intent(this, NewGameActivity.class);
+    	startActivity(intent);
     }
     
 }
---
+++ src/com/gradugation/NewGameActivity.java
@@ -0,0 +1,41 @@
+package com.gradugation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class NewGameActivity extends Activity {
+
+	public static final String NUMBER_OF_PLAYERS = "com.gradugation.number_of_players";
+	public static final int MAX_NUMBER_PLAYERS = 1;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_game, menu);
+		return true;
+	}
+	
+	public void chooseCharacter(View view) {
+		Intent intent = new Intent(this, ChooseCharacterActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.number_of_players);
+    	int numberOfPlayers = Integer.parseInt(editText.getText().toString());
+    	
+    	if (numberOfPlayers > MAX_NUMBER_PLAYERS || numberOfPlayers <= 0) {
+    		editText.setError("Invalid number of players.");
+    		return;
+    	}
+    	
+    	intent.putExtra(NUMBER_OF_PLAYERS, numberOfPlayers);
+    	startActivity(intent);
+	}
+
+}

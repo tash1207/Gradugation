f3b5fadc9e75f66ca454b00aaf52a70c916c5695
Ashley Sarver <asarver1@gmail.com>
2013-10-22 16:43:35 -0400

    cleaned up some code, put updating gui in handler, added touch even

--- src/com/gradugation/WhackAFlyerGame.java
+++ src/com/gradugation/WhackAFlyerGame.java
@@ -10,39 +10,45 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.scene.background.RepeatingSpriteBackground;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
-import org.andengine.opengl.texture.ITexture;
-import org.andengine.opengl.texture.Texture;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
-import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.adt.color.Color;
 
+import android.os.Handler;
 import android.view.Menu;
 
-public class WhackAFlyerGame extends SimpleBaseGameActivity {
+public class WhackAFlyerGame extends SimpleBaseGameActivity implements IOnSceneTouchListener {
 
 	private static final int CAMERA_WIDTH = 480;
 	private static final int CAMERA_HEIGHT = 320;
-	
-	private ITexture bgTexture;
-	private ITexture mFaceTexture;
+
 	private BitmapTextureAtlas characterTextureAtlas;
     public ITextureRegion character;
-    public ITextureRegion bgTextureRegion;
-    private BitmapTextureAtlas mParallaxBackAtlas;
-    private ITextureRegion mParallaxBackRegion;
+    private BitmapTextureAtlas bgTextureAtlas;
+    private ITextureRegion bgRegion;
     private Sprite sprImage;
     private float currentX;
     private float currentY;
-    private RepeatingSpriteBackground mRepeatingBackground;
+    final Scene scene = new Scene();
+    
+ // Need handler for callbacks to the UI thread
+    final Handler mHandler = new Handler();
+
+    // Create runnable for posting
+    final Runnable mUpdateResults = new Runnable() {
+        public void run() {
+            updateResultsInUi();
+        }
+    };
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,68 +72,77 @@ public class WhackAFlyerGame extends SimpleBaseGameActivity {
 		this.character = BitmapTextureAtlasTextureRegionFactory.createFromAsset(characterTextureAtlas, this, "splash2.png", 0, 0);;
 		this.characterTextureAtlas.load();
 		
-		this.mParallaxBackAtlas = new BitmapTextureAtlas(this.getTextureManager(), 951,720,TextureOptions.NEAREST_PREMULTIPLYALPHA);
-		this.mParallaxBackRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mParallaxBackAtlas, this, "bricks.png", 0, 0);;
-		this.mParallaxBackAtlas.load();
+		this.bgTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 951,720,TextureOptions.NEAREST_PREMULTIPLYALPHA);
+		this.bgRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bgTextureAtlas, this, "bricks.png", 0, 0);;
+		this.bgTextureAtlas.load();
 		
 	}
-	
-	/*protected void onLoadResources() throws IOException {
-		this.mParallaxBack = new AssetBitmapTexture(this.getTextureManager(), this.getAssets(), "drawable/bricks.png", TextureOptions.BILINEAR);
-		this.mParallaxBackRegion = TextureRegionFactory.extractFromTexture(this.mParallaxBack);
-		this.mParallaxBack.load();
-	}*/
+ 
 
 	@Override
 	protected Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
+		
+		scene.setOnSceneTouchListener(this);
 		
 		final float centerX = 239;
 		final float centerY = 0;
 		currentX = centerX;
 		currentY = centerY;
 
-		final Scene scene = new Scene();
+		
 		
 		scene.setBackground(new Background(Color.WHITE));
 		   
-		  sprImage = new Sprite(0, 0, mParallaxBackRegion, this.getVertexBufferObjectManager());
-		  scene.attachChild(sprImage);
+		final Sprite mySprite = new Sprite(0, 0, bgRegion, this.getVertexBufferObjectManager());
+		
 		  
-		  final Sprite mySprite = new Sprite(currentX,currentY, character, this.getVertexBufferObjectManager());
-			mySprite.setScale((float) .1);
-			scene.attachChild(mySprite);
+		sprImage = new Sprite(currentX,currentY, character, this.getVertexBufferObjectManager());
+		sprImage.setScale((float) .1);
+		scene.attachChild(mySprite);
+		scene.attachChild(sprImage);
+		
+		mHandler.post(mUpdateResults);
 			
-			scene.registerUpdateHandler(new IUpdateHandler() {
-				@Override
-				public void reset() { }
-
-				@Override
-				public void onUpdate(final float pSecondsElapsed) {
-					
-					// need boundries here
-					mySprite.registerEntityModifier(new MoveModifier(0.8f,currentX,currentY, currentX, currentY + 8)
-							{
-								@Override
-						        protected void onModifierStarted(IEntity pItem)
-						        {
-						                super.onModifierStarted(pItem);
-
-								}
-
-						        @Override
-						        protected void onModifierFinished(IEntity pItem)
-						        {
-						        		currentX=mySprite.getX();
-						        		currentY=mySprite.getY();
-						                super.onModifierFinished(pItem);
-
-						        }
-							});
-						}
-						
-			});
+		
 
         return scene;
+	}
+	
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if (pSceneTouchEvent.isActionDown()) {
+			
+		}
+		return false;
+	}
+	
+	public void updateResultsInUi() {
+		scene.registerUpdateHandler(new IUpdateHandler() {
+			@Override
+			public void reset() { }
+
+			@Override
+			public void onUpdate(final float pSecondsElapsed) {		
+				if (currentY <= CAMERA_HEIGHT) {
+					sprImage.registerEntityModifier(new MoveModifier(0.8f,
+							currentX,currentY, currentX, currentY + 8) {
+						@Override
+				    	protected void onModifierStarted(IEntity pItem) {
+				        	super.onModifierStarted(pItem);
+						}
+
+				        @Override
+				        protected void onModifierFinished(IEntity pItem) {
+				        	currentX=sprImage.getX();
+				        	currentY=sprImage.getY();
+				            super.onModifierFinished(pItem);
+				        }
+					});
+				} else {
+					
+				}
+			}
+		});
 	}
 }

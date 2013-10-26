4cd5198c18a8ae22909818f485a696588b37ff6a
Ashley Sarver <asarver1@gmail.com>
2013-10-26 16:05:58 -0400

    added setter functions

--- src/com/coordinates/Coordinate.java
+++ src/com/coordinates/Coordinate.java
@@ -12,4 +12,12 @@ public abstract class Coordinate {
 	public float getY() {
 		return y;
 	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
 }

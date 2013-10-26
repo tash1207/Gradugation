b1ce44839a866e13d999b40af558fb97401b4b5a
Tasha Hankewych <tash1207@bellsouth.net>
2013-10-26 12:52:28 -0400

    created a bus asset that drives onto the screen and lets passengers out (that get in your way), more sprites getting in the way, new hub background images

--- src/com/gradugation/WaitInLineMinigame.java
+++ src/com/gradugation/WaitInLineMinigame.java
@@ -33,6 +33,9 @@ public class WaitInLineMinigame extends SimpleBaseGameActivity implements IOnSce
 	
 	private static int DEVICE_WIDTH;
 	private static int DEVICE_HEIGHT;
+	
+    private BitmapTextureAtlas bgTextureAtlas;
+    private ITextureRegion bgRegion;
 
 	private BitmapTextureAtlas characterTextureAtlas;
     public ITextureRegion character;
@@ -43,23 +46,27 @@ public class WaitInLineMinigame extends SimpleBaseGameActivity implements IOnSce
     private BitmapTextureAtlas engineerTextureAtlas;
     public ITextureRegion engineerTextureRegion;
     
-    private BitmapTextureAtlas bgTextureAtlas;
-    private ITextureRegion bgRegion;
+    private BitmapTextureAtlas busTextureAtlas;
+    public ITextureRegion busTextureRegion;
     
     private Sprite sprChar;
     private Sprite sprBg;
     private Sprite spr1;
     private Sprite spr2;
+    private Sprite sprBus;
     
     // Coordinates of sprites
     private float currentX = 275;
     private float currentY = 15;
     
-    private float spr1X = -50;
-    private float spr1Y = 210;
+    private float spr1X = 65;
+    private float spr1Y = 205;
     
     private float spr2X = 400;
     private float spr2Y = 120;
+    
+    private float sprBusX = 75;
+    private float sprBusY = 0;
     
     final Scene scene = new Scene();
     
@@ -91,30 +98,40 @@ public class WaitInLineMinigame extends SimpleBaseGameActivity implements IOnSce
 	@Override
 	protected void onCreateResources() throws IOException {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-		this.characterTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 512, 512, 
-				TextureOptions.BILINEAR);
-		this.character = BitmapTextureAtlasTextureRegionFactory.createFromAsset(characterTextureAtlas, this, 
-				"splash2.png", 0, 0);
-		this.characterTextureAtlas.load();
-		
-		this.athleteTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 72, 71, 
-				TextureOptions.BILINEAR);
-		this.athleteTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(athleteTextureAtlas, this, 
-				"athlete.png", 0, 0);
-		this.athleteTextureAtlas.load();
-		
-		this.engineerTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 69, 72, 
-				TextureOptions.BILINEAR);
-		this.engineerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(engineerTextureAtlas, this, 
-				"engineer.png", 0, 0);
-		this.engineerTextureAtlas.load();
-		
+		// Background
 		this.bgTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 960, 720, 
 				TextureOptions.NEAREST_PREMULTIPLYALPHA);
 		this.bgRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bgTextureAtlas, this, 
 				"hub.png", 0, 0);
 		this.bgTextureAtlas.load();
 		
+		// Main character
+		this.characterTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 512, 512, 
+				TextureOptions.BILINEAR);
+		this.character = BitmapTextureAtlasTextureRegionFactory.createFromAsset(characterTextureAtlas, this, 
+				"splash2.png", 0, 0);
+		this.characterTextureAtlas.load();
+		
+		// Athletes
+		this.athleteTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 72, 71, 
+				TextureOptions.BILINEAR);
+		this.athleteTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(athleteTextureAtlas, this, 
+				"athlete.png", 0, 0);
+		this.athleteTextureAtlas.load();
+		
+		// Engineers
+		this.engineerTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 69, 72, 
+				TextureOptions.BILINEAR);
+		this.engineerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(engineerTextureAtlas, this, 
+				"engineer.png", 0, 0);
+		this.engineerTextureAtlas.load();
+		
+		// Bus
+		this.busTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 214, 324, 
+				TextureOptions.BILINEAR);
+		this.busTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(busTextureAtlas, this, 
+				"bus.png", 0, 0);
+		this.busTextureAtlas.load();
 	}
 
 	@Override
@@ -131,19 +148,19 @@ public class WaitInLineMinigame extends SimpleBaseGameActivity implements IOnSce
 		
 		spr1 = new Sprite(spr1X, spr1Y, athleteTextureRegion, this.getVertexBufferObjectManager());
 		spr1.setScale(0.8f);
+		spr1.setVisible(false);
 		
 		spr2 = new Sprite(spr2X, spr2Y, engineerTextureRegion, this.getVertexBufferObjectManager());
 		spr2.setScale(0.8f);
+		
+		sprBus = new Sprite(sprBusX, sprBusY, busTextureRegion, this.getVertexBufferObjectManager());
+		sprBus.setScale(0.8f);
 		
 		scene.attachChild(sprBg);
 		scene.attachChild(spr1);
 		scene.attachChild(spr2);
 		scene.attachChild(sprChar);
-		
-//        scene.setBackground(new SpriteBackground(new Sprite(0, 0, DEVICE_WIDTH, DEVICE_HEIGHT, 
-//        		this.bgRegion, getVertexBufferObjectManager())));
-//
-//        scene.setPosition(0, 0);
+		scene.attachChild(sprBus);
 		
 		mHandler.post(mUpdateResults);
 
@@ -166,21 +183,53 @@ public class WaitInLineMinigame extends SimpleBaseGameActivity implements IOnSce
 
 			@Override
 			public void onUpdate(final float pSecondsElapsed) {
-				// Motion for sprite 1
-				spr1.registerEntityModifier(new MoveModifier(0.05f,
-						spr1X, spr1Y, spr1X + 2, spr1Y) {
-					@Override
-			    	protected void onModifierStarted(IEntity pItem) {
-			        	super.onModifierStarted(pItem);
+				if (sprBusY < 180) {
+					// Motion for bus
+					sprBus.registerEntityModifier(new MoveModifier(0.03f,
+							sprBusX, sprBusY, sprBusX, sprBusY + 3) {
+						@Override
+				    	protected void onModifierStarted(IEntity pItem) {
+				        	super.onModifierStarted(pItem);
+						}
+	
+				        @Override
+				        protected void onModifierFinished(IEntity pItem) {
+				        	sprBusX = sprBus.getX();
+				        	sprBusY = sprBus.getY();
+				            super.onModifierFinished(pItem);
+				        }
+					});
+				}
+				else {
+					spr1.setVisible(true);
+					// When sprite 1 is done exiting the bus off the screen, have another "sprite 1"
+					// exit the bus
+					if (spr1X > CAMERA_WIDTH && currentY < CAMERA_HEIGHT)  {
+						spr1X = 65;
+						spr1Y = 220;
 					}
-
-			        @Override
-			        protected void onModifierFinished(IEntity pItem) {
-			        	spr1X = spr1.getX();
-			        	spr1Y = spr1.getY();
-			            super.onModifierFinished(pItem);
-			        }
-				});
+					// Motion for sprite 1
+					spr1.registerEntityModifier(new MoveModifier(0.04f,
+							spr1X, spr1Y, spr1X + 2, spr1Y) {
+						@Override
+				    	protected void onModifierStarted(IEntity pItem) {
+				        	super.onModifierStarted(pItem);
+						}
+	
+				        @Override
+				        protected void onModifierFinished(IEntity pItem) {
+				        	spr1X = spr1.getX();
+				        	spr1Y = spr1.getY();
+				            super.onModifierFinished(pItem);
+				        }
+					});
+				}
+				// When sprite 2 is done walking to the bus, have another "sprite 2"
+				// walk from the top right of the screen
+				if (spr2X < 88 && currentY < CAMERA_HEIGHT)  {
+					spr2X = CAMERA_WIDTH + 100;
+					spr2Y = 285;
+				}
 				// Motion for sprite 2
 				spr2.registerEntityModifier(new MoveModifier(0.03f,
 						spr2X, spr2Y, spr2X - 2, spr2Y) {

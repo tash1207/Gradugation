2e14e51108d35adfc87925bd291cc023554b232f
abukhalilaf <abukhalilaf@ufl.edu>
2013-11-25 13:30:02 -0500

    changed database scheme

--- src/com/gradugation/DbHelper.java
+++ src/com/gradugation/DbHelper.java
@@ -13,19 +13,17 @@ import android.util.Log;
 public class DbHelper extends SQLiteOpenHelper {
         SQLiteDatabase db;
         public static final String DB_NAME = "gradugation";
-        public static final int DB_VERSION = 4;
+        public static final int DB_VERSION = 5;
         
         //Game Table
         public static final String GAMES = "games";
         
         public static final String G_ID = "id";
-        public static final String CHAR_BASE_ID = "char_base_id";
+        public static final String TIME_DATE = "time_date";
         public static final String NUM_OF_PLAYERS = "num_of_players";
         public static final String CURRENT_PLAYER = "current_player";
-        public static final String DATE = "game_date";
-        public static final String TIME = "game_time";
         
-        public static final String[] GAME_TABLE = {G_ID, CHAR_BASE_ID, NUM_OF_PLAYERS, CURRENT_PLAYER, DATE, TIME};
+        public static final String[] GAME_TABLE = {G_ID, TIME_DATE, NUM_OF_PLAYERS, CURRENT_PLAYER};
         
         //Table mapping for switch statments
         public static final int GAME_TABLE_ID = 1;
@@ -122,7 +120,7 @@ public class DbHelper extends SQLiteOpenHelper {
                         "primary key  ("+MINIGAME_GAME_ID+", "+MINIGAME_GID+", "+MINIGAME_CHAR_ID+"));");
                         */
                 db.execSQL("create table if not exists " + GAMES +
-                		"(" + G_ID + " integer, " + CHAR_BASE_ID + " integer, " + NUM_OF_PLAYERS + " integer," + CURRENT_PLAYER + " integer, " + DATE + " text, " + TIME + " text, " +
+                		"(" + G_ID + " integer, " + TIME_DATE + " text, " + NUM_OF_PLAYERS + " integer," + CURRENT_PLAYER + " integer, " +
                         "primary key autoincrement (" + G_ID + "));");
                 db.execSQL("create table if not exists " + CHARACTER +
                 		"(" + CHARACTER_ID + " integer, " + CHARACTER_TYPE + " text, " + CHARACTER_NAME + " TEXT," + X_COORD + " integer, " +
@@ -157,11 +155,9 @@ public class DbHelper extends SQLiteOpenHelper {
                         case GAME_TABLE_ID: 
                                 tableName = GAMES;
                                 values.put(G_ID, tableValues[0]);
-                                values.put(CHAR_BASE_ID, tableValues[1]);
+                                values.put(TIME_DATE, tableValues[1]);
                                 values.put(NUM_OF_PLAYERS, tableValues[2]);
                                 values.put(CURRENT_PLAYER, tableValues[3]);
-                                values.put(DATE, tableValues[4]);
-                                values.put(TIME, tableValues[5]);
                                 break;
                         case CHARACTER_TABLE_ID: 
                                 tableName = CHARACTER;
@@ -216,11 +212,9 @@ public class DbHelper extends SQLiteOpenHelper {
 	                        case GAME_TABLE_ID: 
 	                        		tableName = GAMES;
 	                            	values.put(G_ID, newValues[0]);
-	                            	values.put(CHAR_BASE_ID, newValues[1]);
+	                            	values.put(TIME_DATE, newValues[1]);
 	                            	values.put(NUM_OF_PLAYERS, newValues[2]);
 	                            	values.put(CURRENT_PLAYER, newValues[3]);
-	                            	values.put(DATE, newValues[4]);
-	                                values.put(TIME, newValues[5]);
 	                                db.update(tableName, values, G_ID + " = " + key[0], null);
 	                                break;
 	                        case CHARACTER_TABLE_ID: 
